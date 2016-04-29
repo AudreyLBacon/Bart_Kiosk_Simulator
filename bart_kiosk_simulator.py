b9257c741c404f9cd3404812c36cea64ede6bba7
Audrey Bacon <audreybacon@Audreys-Macbook.local>
2016-04-29 15:49:07 -0700

    Bart Kiost Simulator part 1

---
+++ bart_kiosk_simulator.py
@@ -0,0 +1,33 @@
+
+
+def load_card (load1,load5,load10,load20):
+
+	load1 = 1.00 * load1
+	load5 = 5.00 * load5
+	load10 = 10.00 * load10
+	load20 = 20.00 * load20
+
+	return load1 + load5 + load10 + load20
+
+print load_card(0,0,0,0)
+
+print load_card (0,0,0,9)
+
+print load_card (2,3,0,0)
+
+print load_card (3,1,1,3)
+
+
+
+
+
+
+
+
+'''DUBLIN_TO_POWELL = 6.15
+FRUITVALE_TO_UNION = 3.80
+ORINDA_TO_RICHMOND = 3.35
+HAWARD_TO_CONCORD = 5.20
+FREMONT_TO_COLMA = 6.60
+'''
+
