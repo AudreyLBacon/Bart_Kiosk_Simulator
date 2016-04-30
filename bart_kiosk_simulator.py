115490d0f1d31af8f8acea1023c4a5ff71ef2242
Audrey Bacon <audreybacon@Audreys-Macbook.local>
2016-04-30 13:31:56 -0700

    Bart Kiosk Simulator Part 2

--- bart_kiosk_simulator.py
+++ bart_kiosk_simulator.py
@@ -1,5 +1,12 @@
 
 
+DUBLIN_TO_POWELL = 6.15
+FRUITVALE_TO_UNION_CITY = 3.80
+ORINDA_TO_RICHMOND = 3.35
+HAYWARD_TO_CONCORD = 5.20
+FREMONT_TO_COLMA = 6.60
+
+
 def load_card (load1,load5,load10,load20):
 
 	load1 = 1.00 * load1
@@ -9,17 +16,23 @@ def load_card (load1,load5,load10,load20):
 
 	return load1 + load5 + load10 + load20
 
-print load_card(0,0,0,0)
+'''print load_card(6,0,1,9)
 
 print load_card (0,0,0,9)
 
-print load_card (2,3,0,0)
+print load_card (2,3,0,9)
 
-print load_card (3,1,1,3)
-
-
+print load_card (3,0,0,9)'''
 
 
+def travel_to_destination(fare_price, card_value):
+	if card_value >= fare_price:
+		print "welcome aboard, enjoy your trip"
+	else:
+		print "you need more money"
+
+clipper_card = load_card(1,1,0,1)
+travel_to_destination(FREMONT_TO_COLMA, clipper_card)
 
 
 
